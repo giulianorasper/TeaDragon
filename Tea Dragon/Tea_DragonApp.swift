d300900e49177896698945ff86046ac02d8b1e8f
Giuliano Rasper <rasper.giuliano@gmail.com>
2024-12-06 17:12:48 +0100

    Initial Commit

---
+++ Tea Dragon/Tea_DragonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tea_DragonApp.swift
+//  Tea Dragon
+//
+//  Created by Giuliano Rasper on 06.12.24.
+//
+
+import SwiftUI
+
+@main
+struct Tea_DragonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tea DragonTests/Tea_DragonTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tea_DragonTests.swift
+//  Tea DragonTests
+//
+//  Created by Giuliano Rasper on 06.12.24.
+//
+
+import Testing
+@testable import Tea_Dragon
+
+struct Tea_DragonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
